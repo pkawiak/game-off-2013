2de3cf233388db49f0eaf6c2e09686d379efe69d
Sebastian Maliszczak <sebastian.maliszczak@sabre.com>
2013-11-17 10:41:16 +0100

    Added scores

--- js/grid/score/score.js
+++ js/grid/score/score.js
@@ -4,20 +4,20 @@ window.Grid.score.Score = function (game) {
     this.score = 0;
     this.scoreText = undefined;
 
-    this.preload = function() {
+    this.preload = function () {
         this.game.load.bitmapFont('Bauhaus', 'assets/fonts/font.png', 'assets/fonts/font.xml');
     };
 
-    this.init = function() {
+    this.init = function () {
         this.score = 0;
     };
 
-    this.create = function() {
-        this.scoreText = this.game.add.bitmapText(200, 0, 'Score', { font: '60px Bauhaus', align: 'center' });
-
+    this.create = function () {
+        this.scoreText = this.game.add.bitmapText(200, 0, 'Score', { font: 'Bauhaus', align: 'center' });
+        console.log(this.scoreText);
     };
 
-    this.update = function() {
+    this.update = function () {
         this.scoreText.setText('Score: ' + Math.round(this.score++));
 
     };
