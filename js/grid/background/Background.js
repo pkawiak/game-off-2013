61834c3e8b135cb286317b7612a6048b0005aa2d
Piotr Kawiak <piotr.kawiak@gmail.com>
2013-11-17 10:34:02 +0100

    namespaces

--- js/grid/background/Background.js
+++ js/grid/background/Background.js
@@ -3,11 +3,11 @@ window.Grid.background.Background = function (game) {
     var background;
 
     this.preload = function () {
-        game.load.image('grid_tile', 'assets/grid.png');
+        game.load.image('grid_tile', 'assets/images/grid_tile.png');
     }
 
     this.create = function () {
-        background = game.add.tileSprite(0, 0, 512, 512, "grid_tile");
+        background = game.add.tileSprite(0, 70, game.width, game.height, "grid_tile");
         background.alpha = 0.5;
         //background.tileScale = new Phaser.Point(0.5, 0.5);
         //background.tilePosition = new Phaser.Point(-64, -64);
