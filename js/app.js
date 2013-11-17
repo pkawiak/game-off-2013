b7b474cf922185f93329ecc306c9dd44faa3beec
Piotr Kawiak <piotr.kawiak@gmail.com>
2013-11-17 09:54:36 +0100

    tile grid

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function () {
     // Create game instance and connect init, create and update methods
     var WIDTH = 512,
-        HEIGHT = 512,
+        HEIGHT = 582,
         game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "Teh Grid", {
             preload: preload,
             create: create,
