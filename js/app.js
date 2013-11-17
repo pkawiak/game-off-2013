04fdf39147228aa457ec7c3f5dd7a3bebbfaa458
Piotr Kawiak <piotr.kawiak@gmail.com>
2013-11-17 09:42:32 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function () {
+    // Create game instance and connect init, create and update methods
+    var WIDTH = 512,
+        HEIGHT = 512,
+        game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, "Teh Grid", {
+            preload: preload,
+            create: create,
+            update: update,
+            init: init
+        });
+
+    function preload() {
+
+    }
+
+    function init() {
+    }
+
+    function create() {
+
+    }
+
+    function update() {
+
+    }
+
+})();
